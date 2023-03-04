d25607d27a2dc77d96f7b6745bd64757d4fc9e63
Marek Wal <walmarek1987@gmail.com>
2023-03-04 08:42:22 +0100

    theme.js created

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  color: {
+    white: "#FFFFFF",
+    ghostWhite: "#FEFEFB",
+    platinum: "#E5E5E5",
+    trueBlue: "#0366D6",
+    gainsboro: "#DADADA",
+    slateGray: "#6E7E91",
+    raisinBlack: "#0366D6",
+  },
+};
