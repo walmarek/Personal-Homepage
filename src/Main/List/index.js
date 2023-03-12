7eeaffe86eed884b9c48c8ee18526655a2e9b865
Marek Wal <walmarek1987@gmail.com>
2023-03-12 16:44:08 +0100

    List added

---
+++ src/Main/List/index.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import styledItemSvg from "../../images/ellipse.svg"
+
+export const List = styled.ul`
+list-style-image: none;
+padding: 0;
+margin-left: 34px;
+`
+export const StyledListItem = styled(styledItemSvg)`
+
+`
+
+export const SkillItem = styled.div`
+
+`
