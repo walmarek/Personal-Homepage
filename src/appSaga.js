df32954c34a5f7254a561f63e27ec4dbcb4e65fc
Marek Wal <walmarek1987@gmail.com>
2023-03-14 09:48:41 +0100

    fetchReposHandler fn created

--- src/appSaga.js
+++ src/appSaga.js
@@ -1,6 +1,18 @@
-import {takeLatest, takeEvery} from "redux-saga/effects"; 
+import { delay, put, call } from "redux-saga/effects";
+import { setLoadingError, setLoadingSuccess, setRepos} from "./appSlice";
+import { getReposFromAPI } from "./Main/Portfolio/getReposFromAPI";
+
+function* fetchReposHandler() {
+  try {
+    const repos = yield call(getReposFromAPI);
+    yield delay(1200);
+    yield put(setRepos(repos));
+    yield put(setLoadingSuccess());
+  } catch (error) {
+    yield put(setLoadingError());
+  }
+};
 
 export function* appSaga() {
-    yield takeLatest()
-    yield takeEvery("*")
+    yield call( fetchReposHandler)
 }
