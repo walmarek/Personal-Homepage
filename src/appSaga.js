a4b3ed027add629635534c2c34bf5c2e06b965ef
Marek Wal <walmarek1987@gmail.com>
2023-03-15 17:20:55 +0100

    delay changed to 1400

--- src/appSaga.js
+++ src/appSaga.js
@@ -1,18 +1,18 @@
 import { delay, put, call } from "redux-saga/effects";
-import { setLoadingError, setLoadingSuccess, setRepos} from "./appSlice";
+import { setLoadingError, setLoadingSuccess, setRepos } from "./appSlice";
 import { getReposFromAPI } from "./Main/Portfolio/getReposFromAPI";
 
 function* fetchReposHandler() {
   try {
     const repos = yield call(getReposFromAPI);
-    yield delay(1200);
+    yield delay(1400);
     yield put(setRepos(repos));
     yield put(setLoadingSuccess());
   } catch (error) {
     yield put(setLoadingError());
   }
-};
+}
 
 export function* appSaga() {
-    yield call( fetchReposHandler)
+  yield call(fetchReposHandler);
 }
