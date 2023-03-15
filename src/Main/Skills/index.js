5da8f47e3394e7624f9cee0dbdd99c6cd010cfee
Marek Wal <walmarek1987@gmail.com>
2023-03-15 17:33:55 +0100

    Skills component created

--- src/Main/Skills/index.js
+++ src/Main/Skills/index.js
@@ -2,20 +2,20 @@ import useToggleDarkTheme from "../../useToggleDarkTheme";
 import { Box } from "../Box";
 import { EllipseIcon, Item, List, SkillsTitle } from "./styled";
 
-export const SkillsList = ({techSkills, title}) => {
-const darkTheme =useToggleDarkTheme()
+export const Skills = ({ techSkills, title }) => {
+  const darkTheme = useToggleDarkTheme();
 
- const skills = techSkills.map((item, index) => 
-<Item key={index}>
-<EllipseIcon/>{item.skill}
-</Item>
- )
+  const skills = techSkills.map((item, index) => (
+    <Item key={index}>
+      <EllipseIcon toggleDarkMode={darkTheme} />
+      {item.skill}
+    </Item>
+  ));
 
   return (
     <Box toggleDarkMode={darkTheme}>
       <SkillsTitle toggleDarkMode={darkTheme}>{title}</SkillsTitle>
-      <List>{skills}
-      </List>
+      <List>{skills}</List>
     </Box>
-  )
+  );
 };
