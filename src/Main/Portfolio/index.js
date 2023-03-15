58a3f3552bb5f765c6ff09a61157228f84bea241
Marek Wal <walmarek1987@gmail.com>
2023-03-15 17:31:31 +0100

    component name changed to RepoTile

--- src/Main/Portfolio/index.js
+++ src/Main/Portfolio/index.js
@@ -1,44 +1,62 @@
 import { useSelector } from "react-redux";
-import { selectRepos } from "../../appSlice";
+import { selectRepos, selectLoadingState } from "../../appSlice";
 import useToggleDarkTheme from "../../useToggleDarkTheme";
 import { Loader } from "./Loader";
+import { PortfolioContent } from "./PortfolioContent";
 import { PortfolioHeader } from "./PortfolioHeader";
 import {
   RepoLink,
-  PortfolioContent,
   PortfolioList,
   RepoDescription,
-  RepoItem,
   RepoLinks,
-  RepoName,
+  RepoTile,
+  RepoTitle,
 } from "./styled";
 
 export const Portfolio = () => {
   const darkTheme = useToggleDarkTheme();
   const repos = useSelector(selectRepos);
 
+  const loadingState = useSelector(selectLoadingState);
+
   return (
     <PortfolioContent>
       <PortfolioHeader />
       <Loader toggleDarkMode={darkTheme} />
-      <PortfolioList>
-        {repos.map((repo) => (
-          <RepoItem key={repo.id}>
-            <RepoName>{repo.name.replaceAll("-", " ")}</RepoName>
-            <RepoDescription>{repo.description}</RepoDescription>
-            <RepoLinks>
-              Demo:
-              <RepoLink href={repo.homepage} rel="me" target="_blank">
-                 {repo.homepage.replaceAll("https://", "")}
-              </RepoLink>
-              Code:
-              <RepoLink href={repo.html_url} rel="me" target="_blank">
-                {repo.html_url.replaceAll("https://", "")}
-              </RepoLink>
-            </RepoLinks>
-          </RepoItem>
-        ))}
-      </PortfolioList>
+      {loadingState === "success" ? (
+        <PortfolioList toggleDarkMode={darkTheme}>
+          {repos.map((repo) => (
+            <RepoTile toggleDarkMode={darkTheme} key={repo.id}>
+              <RepoTitle toggleDarkMode={darkTheme}>
+                {repo.name.replaceAll("-", " ")}
+              </RepoTitle>
+              <RepoDescription>{repo.description}</RepoDescription>
+              <RepoLinks>
+                Demo:
+                <RepoLink
+                  toggleDarkMode={darkTheme}
+                  href={repo.homepage}
+                  rel="me"
+                  target="_blank"
+                >
+                  {repo.homepage.replaceAll("https://", "")}
+                </RepoLink>
+                Code:
+                <RepoLink
+                  toggleDarkMode={darkTheme}
+                  href={repo.html_url}
+                  rel="me"
+                  target="_blank"
+                >
+                  {repo.html_url.replaceAll("https://", "")}
+                </RepoLink>
+              </RepoLinks>
+            </RepoTile>
+          ))}
+        </PortfolioList>
+      ) : (
+        ""
+      )}
     </PortfolioContent>
   );
 };
