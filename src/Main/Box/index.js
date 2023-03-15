c9199356f9d1df21a65eedba08e35f43923bb0b4
Marek Wal <walmarek1987@gmail.com>
2023-03-15 17:30:45 +0100

    staling added

--- src/Main/Box/index.js
+++ src/Main/Box/index.js
@@ -1,19 +1,22 @@
 import styled, { css } from "styled-components";
 
-
 export const Box = styled.div`
-max-width: 1219px;
-box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
-border-radius: 4px;
-background: ${({ theme }) => theme.color.white};
+  max-width: 1219px;
+  box-shadow: 0px 16px 58px 0px ${({ theme }) => theme.color.cetaceanBlue}08,
+    0px -2px 50px 0px ${({ theme }) => theme.color.cetaceanBlue}05;
+  border-radius: 4px;
+  background: ${({ theme }) => theme.color.white};
 
-margin: 73px 0 72px 0;
-padding: 0;
-transition: 0.6s ease-in ;
+  margin: 73px 0 72px 0;
+  padding: 0;
+  transition: 0.4s ease-in;
 
-${({toggleDarkMode}) => toggleDarkMode && css`
-background: ${({ theme }) => theme.color.mineShaft};
-color: ${({ theme }) => theme.color.white};
-`}
-
-`
+  ${({ toggleDarkMode }) =>
+    toggleDarkMode &&
+    css`
+      background: ${({ theme }) => theme.color.mineShaft};
+      color: ${({ theme }) => theme.color.white};
+      box-shadow: 0px 16px 58px 0px ${({ theme }) => theme.color.cetaceanBlue}08;
+      box-shadow: 0px -2px 50px 0px ${({ theme }) => theme.color.cetaceanBlue}05;
+    `}
+`;
---
+++ src/Main/Portfolio/PortfolioContent/index.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const PortfolioContent = styled.div`
+  display: grid;
+  grid-template-columns: auto;
+  justify-items: center;
+  margin: 0;
+`;
