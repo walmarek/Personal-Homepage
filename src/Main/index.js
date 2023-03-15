80cdedc22230bd789c2a7924d332bb6992eaa958
Marek Wal <walmarek1987@gmail.com>
2023-03-15 17:29:55 +0100

    code formated

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,17 +1,24 @@
 import useToggleDarkTheme from "../useToggleDarkTheme";
 import { Portfolio } from "./Portfolio";
-import { SkillsList } from "./SkillsList";
+import { Skills } from "./Skills";
 import { mySkills, toLearnSkills } from "./techSkills";
 
-
 export const Main = () => {
-const darkTheme = useToggleDarkTheme()
+  const darkTheme = useToggleDarkTheme();
 
   return (
     <>
-      <SkillsList toggleDarkMode={darkTheme} techSkills={mySkills} title="My skillset includes 🛠️"/>
-      <SkillsList toggleDarkMode={darkTheme} techSkills={toLearnSkills} title="What I want to learn next 🚀"/>
-      <Portfolio toggleDarkMode={darkTheme}/>
+      <Skills
+        toggleDarkMode={darkTheme}
+        techSkills={mySkills}
+        title="My skillset includes 🛠️"
+      />
+      <Skills
+        toggleDarkMode={darkTheme}
+        techSkills={toLearnSkills}
+        title="What I want to learn next 🚀"
+      />
+      <Portfolio toggleDarkMode={darkTheme} />
     </>
   );
 };
