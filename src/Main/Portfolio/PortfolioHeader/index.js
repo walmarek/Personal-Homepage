9919f832356ad2b0f3e9a398ee8dce60a0677c4a
Marek Wal <walmarek1987@gmail.com>
2023-03-15 17:33:03 +0100

    toggleDarkMode added

--- src/Main/Portfolio/PortfolioHeader/index.js
+++ src/Main/Portfolio/PortfolioHeader/index.js
@@ -1,14 +1,13 @@
 import { GitHubIcon, PortfolioTitle, UnderTitle } from "./styled";
 import useToggleDarkTheme from "../../../useToggleDarkTheme";
-import { PortfolioContent } from "../styled";
-
+import { PortfolioContent } from "../PortfolioContent";
 
 export const PortfolioHeader = () => {
-const darkTheme = useToggleDarkTheme()
+  const darkTheme = useToggleDarkTheme();
 
   return (
     <PortfolioContent>
-      <GitHubIcon />
+      <GitHubIcon toggleDarkMode={darkTheme} />
       <PortfolioTitle toggleDarkMode={darkTheme}>Portfolio</PortfolioTitle>
       <UnderTitle toggleDarkMode={darkTheme}>My recent projects</UnderTitle>
     </PortfolioContent>
