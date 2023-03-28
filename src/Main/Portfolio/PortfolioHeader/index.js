d14f6b926ac0160091e4eb91e7007fbca0360101
Marek Wal <walmarek1987@gmail.com>
2023-03-28 15:54:06 +0200

    lightTheme & darkTheam created

--- src/Main/Portfolio/PortfolioHeader/index.js
+++ src/Main/Portfolio/PortfolioHeader/index.js
@@ -1,14 +1,12 @@
-import useToggleDarkTheme from "../../../useToggleDarkTheme";
 import { GitHubIcon, PortfolioTitle, UnderTitle } from "./styled";
 
 export const PortfolioHeader = () => {
-  const darkTheme = useToggleDarkTheme();
 
   return (
     <>
-      <GitHubIcon toggleDarkMode={darkTheme} />
-      <PortfolioTitle toggleDarkMode={darkTheme}>Portfolio</PortfolioTitle>
-      <UnderTitle toggleDarkMode={darkTheme}>My recent projects</UnderTitle>
+      <GitHubIcon/>
+      <PortfolioTitle>Portfolio</PortfolioTitle>
+      <UnderTitle>My recent projects</UnderTitle>
     </>
   );
 };
