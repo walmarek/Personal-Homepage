23428b421e9239b88403ac99c9bc1955552fead0
Marek Wal <walmarek1987@gmail.com>
2023-03-28 15:54:54 +0200

    lightTheme & darkTheme created

--- src/Main/Skills/index.js
+++ src/Main/Skills/index.js
@@ -1,21 +1,19 @@
-import useToggleDarkTheme from "../../useToggleDarkTheme";
 import { Box } from "../Box";
 import { EllipseIcon, Item, List, SkillsTitle } from "./styled";
 
 export const Skills = ({ techSkills, title }) => {
-  const darkTheme = useToggleDarkTheme();
-
-  const skills = techSkills.map((item, index) => (
-    <Item key={index}>
-      <EllipseIcon toggleDarkMode={darkTheme} />
-      {item.skill}
-    </Item>
-  ));
 
   return (
-    <Box toggleDarkMode={darkTheme}>
-      <SkillsTitle toggleDarkMode={darkTheme}>{title}</SkillsTitle>
-      <List>{skills}</List>
+    <Box>
+      <SkillsTitle>{title}</SkillsTitle>
+      <List>
+        {techSkills.map((skill) => (
+          <Item key={skill}>
+            <EllipseIcon/>
+            {skill}
+          </Item>
+        ))}
+      </List>
     </Box>
   );
 };
