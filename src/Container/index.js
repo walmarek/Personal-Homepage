790ff757af112e922af3bd93f9d4b6fe01a18d75
Marek Wal <walmarek1987@gmail.com>
2023-03-29 20:52:42 +0200

    semantic HTML implemented

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.section`
   background: ${({ theme }) => theme.color.containerBackground};
   display: grid;
   grid-template-columns: auto auto;
