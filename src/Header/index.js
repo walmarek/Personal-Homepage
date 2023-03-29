65de29e546caa6a0b88e228a8fc07d2efe0fa11a
Marek Wal <walmarek1987@gmail.com>
2023-03-29 20:55:38 +0200

    email.js added

--- src/Header/index.js
+++ src/Header/index.js
@@ -12,6 +12,7 @@ import myImage from "../images/marek-wal-photo.jpg";
 import { Button } from "../Button";
 import { ToggleDarkMode } from "./ToggleDarkMode";
 import { Link } from "../Link";
+import { email } from "../email";
 
 export const Header = () => {
   return (
@@ -25,7 +26,7 @@ export const Header = () => {
             👨🏻💻I'm enthusiastic & ambitious Trainee Frontend Developer with
             great passion for IT, currently looking for new job opportunities.
           </ContainerInfo>
-          <Link href={"mailto: walmarek1987@gmail.com"}>
+          <Link href={`mailto:${email}`} title={email}>
             <Button>
               <EmailIcon />
               <Span>Hire Me</Span>
